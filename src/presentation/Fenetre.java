3ba46d67164d1d0d5008534926d8dec48f9bf825
izardj <izard.jerome@free.fr>
2017-03-31 16:09:02 +0200

    final

--- src/presentation/Fenetre.java
+++ src/presentation/Fenetre.java
@@ -5,7 +5,6 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Collection;
 import java.util.List;
 
 import javax.swing.BoxLayout;
